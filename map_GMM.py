5ba6455959e6071fc9b310d7b6e6df4145e636e7
carstencww <carstencww@gmail.com>
2018-04-19 16:59:29 +0800

    fix mapper

--- map_GMM.py
+++ map_GMM.py
@@ -26,11 +26,13 @@ if __name__ == '__main__':
 			origin_mu[class_no] = [float(x) for x in mu]
 			origin_cov[class_no] = np.asarray([float(x) for x in cov]).reshape(25,25)
 	origin_mu = np.asarray(origin_mu)
-	#print(origin_mu[0].shape)
-	#print(origin_cov[0].shape)
-	q_numer_sum = np.zeros((10,origin_mu.shape[1]))
+
+
+	mu_numer_sum = [np.zeros(origin_mu[0].shape) for x in range(10)]
 	pi_numer_sum = [0] * 10
-	cov_numer_sum = [0] * 10
+	cov_numer_sum = [np.zeros(origin_cov[0].shape) for x in range(10)]
+	#print(mu_numer_sum[0].shape)
+	#print(cov_numer_sum[0].shape)
 	for line in sys.stdin:
 		partial_gamma = [0]*10
 		cnt+=1
@@ -38,20 +40,22 @@ if __name__ == '__main__':
 		coords = line.split(",")
 		coords = [float(x) for x in coords]
 		coords = np.asarray(coords)
-		print(coords.shape)
 		for i in range(0,10):
-			partial_gamma[i] = multinorm(coords,origin_mu[i],origin_cov[i])
+			partial_gamma[i] = origin_pi[i] * multinorm(coords,origin_mu[i],origin_cov[i])
 	
 		total = sum(partial_gamma)
 
 		gamma = [x / total for x in partial_gamma]	
-		print(gamma)
+		#print(gamma)
 		for i in range(0,10):
 			pi_numer_sum[i] += gamma[i]
-			q_numer_sum[i] += gamma[i] * coords
-		
-	#for i in range(0,10):
-	#	print(str(i)+"\t"+str(cnt)+":"+str(pi_numer_sum[i])+":"+",".join(str(x) for x in q_numer_sum[i]))
+			mu_numer_sum[i] += gamma[i] * coords
+			cov_numer_sum[i] += gamma[i] * np.matmul(np.reshape(coords - origin_mu[i],(-1,1)),np.reshape(coords - origin_mu[i],(1,-1)))
+	#print(pi_numer_sum)
+	#print(cov_numer_sum)
+	for i in range(0,10):
+		covmat = np.reshape(cov_numer_sum[i],-1)
+		print(str(i)+"\t"+str(cnt)+":"+str(pi_numer_sum[i])+":"+",".join(str(y) for y in covmat)+":"+",".join(str(x) for x in mu_numer_sum[i]))
 
 
 
